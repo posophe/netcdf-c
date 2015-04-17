44aaa25ac148fe4f7cffc69bc32fe1281bd39662
dmh <dmh@unidata.ucar.edu>
2015-04-17 16:01:47 -0600

    attempt to get cmake to work

--- cf.cmake
+++ cf.cmake
@@ -1,14 +1,20 @@
+#!/bin/bash
+
+CPPATH="t:/cygwin/usr/local"
+
 rm -fr build
 mkdir build
 cd build
-UL=/usr/local
-PPATH="$UL"
-HDF5="-DHDF5_LIB=${UL}/lib/libhdf5.so -DHDF5_HL_LIB=${UL}/lib/libhdf5_hl.so -DHDF5_INCLUDE_DIR=${UL}/include"
-FLAGS="-DCMAKE_PREFIX_PATH=$PPATH"
-FLAGS="$FLAGS -DCMAKE_INSTALL_PREFIX=${UL}"
-FLAGS="$FLAGS -DCMAKE_PREFIX_PATH="$PPATH"
-FLAGS="$FLAGS -DENABLE_DAP_REMOTE_TESTS=true
-FLAGS="$FLAGS -DENABLE_DAP_AUTH_TESTS=true"
-cmake $FLAGS ${HDF5} ..
+
+FLAGS="-DCMAKE_PREFIX_PATH=${CPPATH}"
+FLAGS="$FLAGS -DCMAKE_INSTALL_PREFIX=${PREFIX}"
+FLAGS="$FLAGS -DBUILD_SHARED_LIBS=ON"
+FLAGS="$FLAGS -DENABLE_DAP_REMOTE_TESTS=true"
+#FLAGS="$FLAGS -DENABLE_DAP_AUTH_TESTS=true"
+FLAGS="$FLAGS -DZLIB_LIBRARY=${CPPPATH}/lib/libzlib.dll.a"
+FLAGS="$FLAGS -DCURL_LIBRARY=${CPPPATH}/lib/libcurl.dll.a"
+
+cmake $FLAGS ..
 cmake --build .
-make test
+cmake --build --target test
+
